cf0f909a2be501e4b47d18c411f45c8d89dd1370
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2022-12-22 14:59:01 -0600

    f commit

---
+++ resources/js/app.js
@@ -0,0 +1,29 @@
+import Dropzone from "dropzone";
+import { drop } from "lodash";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone',{
+    dictDefaultMessage: 'Sube aquí tu imagen',
+    acceptedFiles: ".png, .jpg, .jpeg, .gif",
+    addRemoveLinks: true,
+    dictRemoveFile: "Borrar Archivo",
+    maxFiles: 1,
+    uploadMultiple: false,
+})
+
+dropzone.on('sending', function(file, xhr, formData){
+    console.log(formData);
+});
+
+dropzone.on('success', function(file, response) {
+    console.log(response);
+})
+
+dropzone.on('error', function(file, message) {
+    console.log(message);
+})
+
+dropzone.on('removedfile', function(file) {
+    console.log("Archivo eliminado");
+})
