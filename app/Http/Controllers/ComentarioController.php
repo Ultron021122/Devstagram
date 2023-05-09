c6e8912b0a43eb448f8f6c683f4b39f7b8bd92ee
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2023-05-09 01:11:55 -0500

    version 3.4.2

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comentario;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
-        
+        // validar
+        $this->validate($request, [
+            'comentario' => 'required|max:255'
+        ]);
+        // almacenar el resultado
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' => $request->comentario
+        ]);
+        // imprimir el mensaje
+        return back()->with('mensaje', 'Comentario Realizado Correctamente');
     }
 }
